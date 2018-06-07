52e712f8e350fda0e18a507f6cef508de9f689cc
ichiro.kirameki <ec2-user@ip-172-31-29-145.us-east-2.compute.internal>
2018-06-07 08:09:10 +0000

    kadai1-1

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -17,11 +17,22 @@ class TasksController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
 
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+            $data += $this->counts($user);
+            return view('welcome', $data);
+        }
+        else {
+            return view('welcome');
+        }
     }
 
     /**
@@ -47,17 +58,18 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required|max:10',  
             'content' => 'required|max:191',
+            'status' => 'required|max:191',            
         ]);
 
-        $task = new Task;
-        $task->status = $request->status; 
-        $task->content = $request->content;
-        $task->save();
+        $request->user()->tasks()->create([
+            'content' => $request->content,
+            'status' => $request->status,
+        ]);
 
         return redirect('/');
     }
+ 
 
     /**
      * Display the specified resource.
@@ -117,10 +129,14 @@ class TasksController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+
+        public function destroy($id)
     {
-        $task = Task::find($id);
-        $task->delete();
+        $task = \App\Task::find($id);
+
+        if (\Auth::user()->id === $task->user_id) {
+            $task->delete();
+        }
 
         return redirect('/');
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+    @if (Auth::check())
+    <?php $user = Auth::user(); ?>
+    
+      @include('tasks.index', ['tasks' => $tasks])
+    @else
     <div class="center jumbotron">
         <div class="text-center">
             <h1>Welcome to the ToDo list</h1>
             {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
+    @endif
 @endsection
+
+
+
