d5882e8c26fc30b55d4b16faead9110f8c57af69
ichiro.kirameki <ec2-user@ip-172-31-29-145.us-east-2.compute.internal>
2018-06-08 03:23:38 +0000

    modify auth1-2

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -84,7 +84,7 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        $task = Task::find($id);
+        $task = \App\Task::find($id);
      if(\Auth::user()->id === $task->user_id){
     
 
@@ -105,7 +105,8 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        $task = Task::find($id);
+        $task = \App\Task::find($id);
+        
         if(\Auth::user()->id === $task->user_id){
 
         return view('tasks.edit', [
